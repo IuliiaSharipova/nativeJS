fdc67de497bad526df58d1930ebb6ebbb7c2a2d4
Iuliia <sharipova.y.s@gmail.com>
2022-08-24 20:33:41 +0200

    add comments to second lesson

--- main1&2.js
+++ main1&2.js
@@ -11,30 +11,35 @@ const arr2 = arr //не создан новый массив, присвоена
 //#2 lesson
 const students = [
     {
+        id: 1,
         name: "Bob",
         age: 22,
         isMarried: true,
         scores: 85,
     },
     {
+        id: 2,
         name: "Alex",
         age: 21,
         isMarried: true,
         scores: 89
     },
     {
+        id: 3,
         name: "Nick",
         age: 20,
         isMarried: false,
         scores: 120
     },
     {
+        id: 4,
         name: "John",
         age: 19,
         isMarried: false,
         scores: 100
     }
 ];
+
 const getNames = (arr) => {
     const result = [];
     //
@@ -48,8 +53,22 @@ const getNames = (arr) => {
 }
 console.log(getNames(students))
 
+const getGreetings = (arr) => {
+    const result = []
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = `Hi, I am ${arr[i].name}`
+        result[i] = newValue
+    }
+    return result
+}
+console.log(getGreetings(students))
+
+
 const addScores = (arr) => {
     const result = []
+    //1.Взять каждый элемент исходного массива
+    //2. Получить из него новое значение
+    //3. Поместить значение в новый массив
     //
     const getNewValue = (el) => ({...el, scores: el.scores + 10})
     //
@@ -64,6 +83,7 @@ console.log(addScores(students))
 //Повторяется все кроме функции call-back, поэтому делаем ее параметром
 const getName = (el) => el.name
 const addScoresForStudents = (el) => ({...el, scores: el.scores + 10})
+
 const getMappedArray = (arr, mapFn) => {
     const result = []
     for (let i = 0; i < arr.length; i++) {
@@ -74,13 +94,17 @@ const getMappedArray = (arr, mapFn) => {
 }
 console.log(getMappedArray(students,
     getName))
+console.log(students.map(getName))
 console.log(getMappedArray(students, addScoresForStudents))
 
 const getFilteredArray = (arr, filterFn) => {
+    //1.Взять каждый элемент исходного массива
+    //2. Проверить каждый элемент на соответствие условию
+    //3. Если соответствует, поместить элемент в новый массив
     const result = []
     for (let i = 0; i < arr.length; i++) {
-        const newValue = filterFn(arr[i])
-        if (newValue === true) {
+        const newValue = filterFn(arr[i]) //true or false
+        if (newValue === true) {  //newValue && result.push(arr[i])
             result.push(arr[i])
         }
     }
@@ -88,12 +112,15 @@ const getFilteredArray = (arr, filterFn) => {
 }
 console.log(getFilteredArray(students, st => st.age > 20))
 
-const getStudent=(arr,findFn)=>{
-    for (i=0; i<arr.length;i++){
+const selfMadeFilter = (arr, findFn) => {
+    for (i = 0; i < arr.length; i++) {
         const newValue = findFn(arr[i])
         if (newValue === true) {
             return arr[i]
         }
     }
 }
-console.log(getStudent(students, st=>st.name === 'Nick')) //аналог find. возвращает первый элемент, удовлетворяющий условиям. не создается новый массив
+console.log(selfMadeFilter(students, st => st.name === 'Nick'))
+//аналог find. возвращает первый элемент, удовлетворяющий условиям.
+// не создается новый массив
+console.log(students.find(st => st.name === 'Nick'))
